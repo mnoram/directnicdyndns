1c56d266574e5cb98fce44ebb9078d8a9fdeba34
Tor Houghton <th-github@bogus.net>
2024-01-31 23:45:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# what it is
+A script for updating dynamic DNS (dyndns) records at Directnic. If you are a user of a domain with dynamic records at Directnic, then this script may be for you.
+
+# usage
+Place your secret (the 64 byte hex value between gateway/ and /?data in Directnic's provided unique URL) in the script's .rc file (_$HOME/.ddrc_), e.g.:
+```
+$ cat ~/.ddrc
+KEY=6f751f5d0b866de67c18ce310ecc2ef5d64b18152b797c392c2cf6c984f9c564
+```
+
+Ensure that the file mode on .ddrc is correct (0400 or 0600); the script will complain if it is not. Once the above has been completed, run the script.
+
+You can read more about Directnic's dynamic DNS service here: https://directnic.com/knowledge#/knowledge/article/3726.
+
+# dependencies
+On OpenBSD there are no dependencies (assuming a recent version of OpenBSD), while Linux needs _curl_ installed.
+
+# compatibility
+It has been written and tested for OpenBSD and Linux.
---
+++ dndnsu.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+## (c) 2024 th@bogus.net
+## Dynamic DNS updater for DirectNIC, uses dyndns.org to obtain your public IP
+##
+RCFILE="$HOME/.ddrc" # this is where you put your secret (KEY=<hex_string>)
+OS=$(uname)
+if [ -e $RCFILE ];then
+    if [ $OS == "OpenBSD" ];
+    then
+        filemode=$(stat -nr "$RCFILE" | cut -d " " -f3 | tail -c 4)
+    else
+        filemode=$(stat -c "%a" "$RCFILE")
+    fi
+    if [[ "$filemode" == "400" || "$filemode" == "600" ]] 
+    then
+        KEY=$(grep "^KEY=" "$RCFILE" | cut -d '=' -f2)
+    else
+        echo "Fatal: wrong permissions on $RCFILE ($filemode), change to 0400 or 0600"
+        exit 1
+    fi
+else
+    echo "Fatal: $RCFILE does not exist, exiting."
+    exit 1
+fi
+
+if [ $OS == "OpenBSD" ]
+then
+    IP_ADDRESS=$(ftp -V -M -o - http://checkip.dyndns.org/ | perl -ne 'print $1 if /Current IP Address: (\d+\.\d+\.\d+\.\d+)/') 
+else
+    if [ ! $(which curl) == "" ]
+    then
+        IP_ADDRESS=$(curl -s -o - http://checkip.dyndns.org | grep -oP '(?<=Current IP Address: )\d+\.\d+\.\d+\.\d+')
+    else
+        echo "Fatal: No curl, exiting."
+        exit 1
+    fi
+fi
+
+URL="https://directnic.com/dns/gateway/$KEY/?data=$IP_ADDRESS"
+
+if [ $OS == "OpenBSD" ]
+then
+    RESULT=$(ftp -V-o - $URL 2>&1 /dev/null)
+else
+    RESULT=$(curl -s -o - $URL 2>&1 /dev/null)
+fi
+
+if [[ $RESULT == *'"result":"success"'* ]]
+then
+    exit 0
+else
+    echo "Fatal: Could not update your DNS record: $RESULT"
+    exit 1
+fi
